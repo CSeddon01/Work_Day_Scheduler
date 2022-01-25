fabb6454794f77a5b030c4b61a4b5d09eff03a56
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-01-25 14:21:42 -0600

    added time color changes and notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,70 @@
+// Get Document and set time
+$(document).ready(function () {
+  var now = moment().format("dddd, MMMM Do");
+  var time = moment().format("HH");
+  $("#currentDay").append(now);
 
+  // Add array for addEvent on the scheduler
+  var addEvent = ["", "", "", "", "", "", "", ""];
+  // Access container div from HTML
+  var container = $(".container");
 
+  //    Set up local.storage and save per event added to scheduler
+  if (localStorage.getItem("addEvent")) {
+    addEvent = JSON.parse(localStorage.getItem("addEvent"));
+  }
+  function save(event) {
+    var id = event.target.id;
+    var text = $("#" + id + "textarea").val();
+    addEvent[id] = text;
+    localStorage.setItem("addEvent", JSON.stringify(addEvent));
+  }
 
-function getLocalStorage(key) {
-    let value = localStorage.getItem(key);
-    if (value) {
-        $(`#text${key}`).text(value);
+  //   Change colors depending on time of day
+  for (var i = 0; i < 8; i++) {
+    var colors;
+    if (time == i + 9) {
+      colors = "present";
+    } else if (time < i + 9) {
+      colors = "future";
+    } else {
+      colors = "past";
     }
-}
 
-
-$( document ).ready(function() {
-    // Display date in this format: thursday, September 15th
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    for (let i = 9; i < 18; i++) {
-    
-    // Create the col/rows
-        var row = $(`<div data-time=${i} id='${i}' class="row time-block">`);
-        var col1 = $('<div class="col-sm-2"> <p class="hour">' + setHours(i) + '</p>');
-        var col2 = $(`<div class="col-sm-8 past description"><textarea id=text${i}  placeholder="Add your event here..."></textarea>`);        
-        var col3 = $(`<button class="btn saveBtn col-md-1" id=${i}><i class="fas fa-save"></i></button>`)
-        row.append(col1);
-        row.append(col2);
-        row.append(col3);
-        $(".container").append(row);
-
-        getLocalStorage(i);
+    //   Have time show AM/PM
+    function timeAMPM(hour) {
+      if (hour > 12) {
+        var timeText = hour - 12;
+        return timeText + "pm";
+      } else {
+        return hour + "am";
+      }
     }
 
-    function setHours(hours) {
-        hours = ((hours + 11) % 12 + 1);
-        return hours;
-        // var ampm = hours >= 12 ? 'pm' : 'am';
-        // hours = hours % 8;
-        // hours = hours ? hours : 8;
-        // return hours + ampm;
-    }
-setHours();
+    //   Create Rows and Columms
+    var rowDiv = $('<div class="row time-block"/>');
 
-function textColors(){
-    var currentTime = new Date().getHours();
-    for (var i = 9; i < 18; i++) { 
-     if ($(`#${i}`).data("time") == currentTime){
-        $(`#text${i}`).addClass("present");
-    } else if (currentTime < $(`#${i}`).data("time")) {
-        $(`#text${i}`).addClass( "future");
-    }
-}
-}
+    var col1 = $('<div class="col hour"></div>');
+    col1.text(timeAMPM(i + 9));
 
-setInterval(function() {
-textColors();
-}, 1000);
+    var col2 = $('<div class="col-6"></div>');
+    var textarea = $('<textarea class="form-control textarea"></textarea>');
+    textarea.attr("id", i + "textarea");
+    textarea.val(addEvent[i]);
+    textarea.addClass(colors);
+    col2.append(textarea);
 
-var saveBtn = $('.saveBtn');
-saveBtn.on('click', function(){
-let eventId = $(this).attr('id');
-let eventText = $(this).parent().siblings().children('.description').val();
-localStorage.setItem(eventId, eventText);
-});});
+    var col3 = $('<div class="col"></div>');
+    var saveButton = $(
+      '<button type="button" class="btn col-md-10 saveBtn fas fa-save"></button'
+    );
+    saveButton.attr("id", i);
+    saveButton.click(function (event) {
+      save(event);
+    });
+    col3.append(saveButton);
+
+    container.append(rowDiv);
+    rowDiv.append(col1, col2, col3);
+  }
+});
